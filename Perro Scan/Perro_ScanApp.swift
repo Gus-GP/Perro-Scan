c2db700af5c81702f0176ded9f6530617a19400e
Gustavo Grinsteins <gusgrinsteins@gmail.com>
2025-09-19 13:15:04 -0600

    Initial Commit

---
+++ Perro Scan/Perro_ScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perro_ScanApp.swift
+//  Perro Scan
+//
+//  Created by Gustavo Grinsteins on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Perro_ScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
